4a02d0b57a423438b552d3ec8097e9b6492e2074
NikVAx <nik.vasilenko1203@gmail.com>
2023-11-28 01:18:54 +0300

    Move state machine from lexer.h to state_machine.h

--- LEXER/lexer.h
+++ LEXER/lexer.h
@@ -6,43 +6,11 @@
 
 #include "transition_info.h"
 #include "lexer_config.h"
+#include "state_machine.h"
 
 #include <string>
 #include <sstream>
 #include <vector>
-
-
-
-class StateMachine {
-protected:
-    StateMachineConfiguration _smConfig;
-
-    static const int IS_BOUNDARY_CODE = 1;
-public:
-    StateMachine(StateMachineConfiguration smConfig)
-        : _smConfig(smConfig)
-        , currentState(smConfig.getInitialState())
-    { }
-
-    int currentState = 1;
-
-    TransitionInfo getTransition(char inputChar) {
-        int inputType = _smConfig.mapInputAlpha(inputChar);
-        int nextState = _smConfig.getNextState(currentState, inputType);
-        bool isBoundary = _smConfig.getBoundary(currentState, inputType) == IS_BOUNDARY_CODE;
-        StatusCodes statusCode = _smConfig.getStatusCode(currentState, inputType);
-
-        return TransitionInfo(inputChar, inputType, nextState, isBoundary, statusCode);
-    }
-
-    int getInitialState() {
-        return _smConfig.getInitialState();
-    }
-
-    void reset() {
-        currentState = _smConfig.getInitialState();
-    }
-};
 
 namespace LEX {
     class Lexer {
